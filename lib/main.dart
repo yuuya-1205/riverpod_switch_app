5fa8710537be264aa866a475b2c33f2f022c59ab
ushi <>
2025-12-07 19:04:44 +0900

    feature: add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:switch_app/firebase_options.dart';
 import 'package:switch_app/view/switch_page.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(ProviderScope(observers: [], child: const MyApp()));
 }
 
