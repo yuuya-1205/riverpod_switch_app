eef129f474340042ebb970a191ada194b16481db
ushi <>
2025-12-07 22:22:31 +0900

    feature: repository

---
+++ lib/data/repository/switch_repository.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:switch_app/data/dto/switch_dto.dart';
+import 'package:switch_app/data/repository/firebase_firestore_repository.dart';
+
+final switchRepositoryProvider = Provider<SwitchRepository>((ref) {
+  return SwitchRepository(ref);
+});
+
+class SwitchRepository {
+  SwitchRepository(this.ref);
+
+  final Ref ref;
+
+  /// 特定のドキュメントIDを取得して、そのドキュメントのisEnabledを取得する。
+  Stream<SwitchDto> subscribeSwitchIsEnabled({required String documentId}) {
+    return ref
+        .read(firebaseFirestoreProvider)
+        .collection('switch')
+        .doc(documentId)
+        .snapshots()
+        .map(
+          (snapshot) =>
+              SwitchDto(isEnabled: snapshot.data()?['isEnabled'] as bool),
+        );
+  }
+
+  Future<void> updateIsEnabled({
+    required String documentId,
+    required bool isEnabled,
+  }) async {
+    await Future.delayed(Duration(seconds: 3));
+    //  throw Exception('test');
+    await ref
+        .read(firebaseFirestoreProvider)
+        .collection('switch')
+        .doc(documentId)
+        .set({'isEnabled': isEnabled});
+  }
+}
