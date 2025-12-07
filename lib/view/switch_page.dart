96256160e403714e412d31e86ed3940652dfa5ad
ushi <>
2025-12-07 22:24:30 +0900

    fix: state.when

--- lib/view/switch_page.dart
+++ lib/view/switch_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:switch_app/logic/switch_notifier.dart';
+import 'package:switch_app/domain/logic/switch_notifier.dart';
 
 class SwitchPage extends ConsumerWidget {
   const SwitchPage({super.key});
@@ -10,40 +10,48 @@ class SwitchPage extends ConsumerWidget {
     final switchState = ref.watch(switchNotifierProvider);
     return Scaffold(
       appBar: AppBar(title: const Text('Switch Page')),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Text(
-                  'enabled',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
+      body: switchState.when(
+        data: (switchState) => Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'enabled',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
 
-                    /// 使用できない場合はトーンダウンしているように見せたいのでグレーで表示
-                    /// 使用できる場合は黒にする
-                    color: switchState.isEnabled
-                        ? Colors.black
-                        : Colors.grey[400],
+                      /// 使用できない場合はトーンダウンしているように見せたいのでグレーで表示
+                      /// 使用できる場合は黒にする
+                      color: switchState.isEnabled
+                          ? Colors.black
+                          : Colors.grey[400],
+                    ),
                   ),
-                ),
-                Spacer(),
-                Switch(
-                  value: switchState.isEnabled,
-                  onChanged: (value) {
-                    switchState.isEnabled
-                        ? ref.read(switchNotifierProvider.notifier).switchOff()
-                        : ref.read(switchNotifierProvider.notifier).switchOn();
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(height: 16.0),
-            if (switchState.isEnabled) const Text('使えるよ！'),
-          ],
+                  Spacer(),
+                  Switch(
+                    value: switchState.isEnabled,
+                    onChanged: (value) {
+                      switchState.isEnabled
+                          ? ref
+                                .read(switchNotifierProvider.notifier)
+                                .switchOff()
+                          : ref
+                                .read(switchNotifierProvider.notifier)
+                                .switchOn();
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              if (switchState.isEnabled) const Text('使えるよ！'),
+            ],
+          ),
         ),
+        error: (error, stackTrace) => Text('Error: $error'),
+        loading: () => const Center(child: CircularProgressIndicator()),
       ),
     );
   }
