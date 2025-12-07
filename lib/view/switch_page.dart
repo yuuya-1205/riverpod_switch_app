5036479bdaf1d79957a6c78fd6e142c6397b2d63
ushi <>
2025-12-07 15:11:15 +0900

    fix: add switch state

--- lib/view/switch_page.dart
+++ lib/view/switch_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:switch_app/logic/switch_notifier.dart';
 
 class SwitchPage extends ConsumerWidget {
   const SwitchPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isEnabled = false;
+    final switchState = ref.watch(switchNotifierProvider);
     return Scaffold(
       appBar: AppBar(title: const Text('Switch Page')),
       body: Padding(
@@ -23,11 +24,20 @@ class SwitchPage extends ConsumerWidget {
 
                     /// 使用できない場合はトーンダウンしているように見せたいのでグレーで表示
                     /// 使用できる場合は黒にする
-                    color: isEnabled ? Colors.black : Colors.grey[400],
+                    color: switchState.isEnabled
+                        ? Colors.black
+                        : Colors.grey[400],
                   ),
                 ),
                 Spacer(),
-                Switch(value: isEnabled, onChanged: (value) {}),
+                Switch(
+                  value: switchState.isEnabled,
+                  onChanged: (value) {
+                    switchState.isEnabled
+                        ? ref.read(switchNotifierProvider.notifier).switchOff()
+                        : ref.read(switchNotifierProvider.notifier).switchOn();
+                  },
+                ),
               ],
             ),
           ],
